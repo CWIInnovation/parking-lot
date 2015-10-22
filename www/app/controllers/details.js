6ea40bbc9aa378a44c23ede5f06237258648b331
Rômulo Alves <romulo0rs@gmail.com>
2015-10-22 20:28:48 -0200

    Ajustando tela de detalhe do veículo

--- www/app/controllers/details.js
+++ www/app/controllers/details.js
@@ -1,8 +1,8 @@
 angular.module('parking-lot').controller('DetailCtrl', ["$scope","$stateParams", "vehiclesService", function ($scope, $stateParams, $vehiclesService) {
     var veiculos = $vehiclesService.getVehicles();
-    
+
     veiculos.forEach(function (veiculo) {
         if (veiculo.licensePlate == $stateParams.licensePlate)
             $scope.ownerInfo = veiculo;
-    });    
+    });
 }]);
