09d86e483cf6938f56b86d2c8f6263eb26d94b3f
Rômulo Alves <romulo0rs@gmail.com>
2015-11-25 23:58:06 -0200

    Adicionando contribuidores do projeto

--- www/app/controllers/about.js
+++ www/app/controllers/about.js
@@ -1,4 +1,14 @@
-angular.module('parking-lot').controller('AboutCtrl', ['$scope',
-    function ($scope) {
+angular.module('parking-lot').controller('AboutCtrl', ['$scope', 'githubService',
+    function ($scope, githubService) {
         $scope.currentDate = new Date();
+        $scope.contributors = [];
+        githubService.get(function (data) {
+            $scope.contributors = data;
+        });
+
+        $scope.openUrl = function (url) {
+            if (!url)
+                return;
+            window.open(url, '_system', 'location=yes');
+        };
     }]);
